ddff6d779af35b667344c02f4faf388d30e2e906
IzoJugal <jugal@izonnet.com>
2025-08-02 13:32:53 +0530

    token verify

--- Model/AuthModel.js
+++ Model/AuthModel.js
@@ -53,7 +53,7 @@ userSchema.methods.generateToken = function () {
       roles: this.roles,
     },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN || "7d" }
+    { expiresIn: process.env.JWT_EXPIRES_IN || "1h" }
   );
 };
 
